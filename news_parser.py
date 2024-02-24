fd70f8cb82d81edfab4735ad74366e7e4b094853
Deco <13683901+Recol@users.noreply.github.com>
2024-02-24 21:10:57 +0000

    update parsing to extract the articles

--- news_parser.py
+++ news_parser.py
@@ -4,31 +4,25 @@ def parse_news_html(html_content: str) -> list:
     soup = BeautifulSoup(html_content, 'html.parser')
     articles = []
 
-    # Find all article containers
-    articles_divs = soup.find_all("div", class_="gs-c-promo")
+    # Find all list items which seem to contain articles
+    list_items = soup.find_all("li", class_="ssrcss-rs7w2i-ListItem")
 
-    for article in articles_divs:
-        title_tag = article.find("h3", class_="gs-c-promo-heading__title")
-        link_tag = article.find("a", class_="gs-c-promo-heading")
-        summary_tag = article.find("p", class_="gs-c-promo-summary")
+    for li in list_items:
+        # Find the link and title within the list item
+        link_tag = li.find("a", class_="ssrcss-afqep1-PromoLink")
+        title_tag = li.find("p", class_="ssrcss-17zglt8-PromoHeadline")
+        summary_tag = li.find("p", class_="ssrcss-1q0x1qg-Paragraph")
 
-        if title_tag and link_tag and summary_tag:
+        if link_tag and title_tag:
             title = title_tag.get_text(strip=True)
             link = "https://www.bbc.co.uk" + link_tag.get('href', '')
-            summary = summary_tag.get_text(strip=True)
-
-        # New code to extract the image source
-        image_tag = article.find("img")
-        image_src = image_tag['src'] if image_tag and image_tag.has_attr('src') else ''
-        image_src = image_src if image_src.startswith('http') else f"https:{image_src}"
-
-        article_info = {
-            "title": title,
-            "url": link,
-            "summary": summary,
-            "image_src": image_src  # Add the image source to the dictionary
-        }
-        articles.append(article_info)
-
+            summary = summary_tag.get_text(strip=True) if summary_tag else ''
+            
+            articles.append({
+                "title": title,
+                "url": link,
+                "summary": summary
+            })
 
     return articles
+
